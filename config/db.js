f89475e7ee7683ce5dec984ba3d22dc414c412f9
Adane Ferede <adaneferede6@gmail.com>
2026-04-19 01:08:13 -0900

    Hardcode database to defaultdb

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'test',
+  database: 'defaultdb',   // <-- FORCE defaultdb
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
